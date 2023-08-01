796a3a47f5774e845cde32d3c80d1ba5573fd08b
Marvin Berrio <marvinccp@gmail.com>
2023-08-01 22:41:07 +0200

    update image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
     <section className="header_container">
       <nav className='nav_container'>
         <Link to={'/'}>
-        <img className='nav_container_image' src="../../../src/assets/images/logo.png" alt="" />
+        <img className='nav_container_image' src="../images/logo.png" alt="logo-angelous" />
         </Link>
         <h4>menu</h4>
       </nav>
