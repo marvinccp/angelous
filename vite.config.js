dcf2af5584ffc38e00830d3846773896d6bcb9a2
Marvin Berrio <marvinccp@gmail.com>
2023-08-07 12:47:53 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
   server: {
     // Configurar el fallback para el enrutamiento del lado del cliente
     historyApiFallback: true,
-  }
-})
+    rewrite: {
+      // Si la solicitud no es para un archivo estático, redirigir a index.html
+      // Esto permitirá que React Router maneje la ruta y muestre el componente correcto
+      // basado en la URL.
+      "^/([^/]*|)(?.*)?$": "/index.html",
+    },
+  },
+});
