f3177e0bd8b48ef0a29ea9bb5a4774d52a6b35eb
Marvin Berrio <marvinccp@gmail.com>
2023-08-07 12:31:30 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Configurar el fallback para el enrutamiento del lado del cliente
+    historyApiFallback: true,
+  }
 })
